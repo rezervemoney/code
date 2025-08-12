233b343b6030ff68157354d06f27d8e60782e49e
Dre Cronye <dre@rezerve.money>
2025-08-12 18:15:21 +0000

    Updated contracts

---
+++ contracts/periphery/lstRZROFTAdapter.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity 0.8.28;
+
+import "../core/AppAccessControlled.sol";
+import "@layerzerolabs/oft-evm/contracts/OFTAdapter.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract lstRZROFTAdapter is OFTAdapter, AppAccessControlled {
+    constructor(address _oft, address _lzEndpoint, address _authority)
+        OFTAdapter(_oft, _lzEndpoint, msg.sender)
+        Ownable(msg.sender)
+    {
+        __AppAccessControlled_init(_authority);
+        _transferOwnership(address(0));
+    }
+
+    function _checkOwner() internal view virtual override {
+        if (!authority.isGovernor(_msgSender())) {
+            revert OwnableUnauthorizedAccount(_msgSender());
+        }
+    }
+
+    function recall(address _token) external onlyGovernor {
+        IERC20(_token).transfer(msg.sender, IERC20(_token).balanceOf(address(this)));
+    }
+
+    function execute(address _to, bytes calldata _data) external onlyGovernor {
+        (bool success,) = _to.call(_data);
+        require(success, "RZROFTAdapter: execute failed");
+    }
+}

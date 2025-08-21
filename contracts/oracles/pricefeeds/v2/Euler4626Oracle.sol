e6c48cbd49e4d5601362a3a975cd84784bf44266
Dre Cronye <dre@rezerve.money>
2025-08-21 17:54:39 +0000

    Updated contracts

---
+++ contracts/oracles/pricefeeds/v2/Euler4626Oracle.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import "../../../interfaces/IOracleV2.sol";
+import "@openzeppelin/contracts/interfaces/IERC4626.sol";
+
+contract Euler4626Oracle is IOracleV2 {
+    IERC4626 public immutable VAULT;
+    IOracleV2 public immutable UNDERLYING_ORACLE;
+
+    IERC20Metadata public immutable asset;
+
+    constructor(IERC4626 _vault, address _underlyingOracle) {
+        VAULT = _vault;
+        UNDERLYING_ORACLE = IOracleV2(_underlyingOracle);
+        asset = IERC20Metadata(address(VAULT));
+        require(address(UNDERLYING_ORACLE.asset()) == address(VAULT.asset()), "Asset mismatch");
+
+        (uint256 rzrAssets, uint256 usdAssets,) = getPriceForAmount(1e18);
+        require(rzrAssets > 0 || usdAssets > 0, "Invalid price");
+    }
+
+    /// @inheritdoc IOracleV2
+    function getPriceForAmount(uint256 shares)
+        public
+        view
+        override
+        returns (uint256 rzrAssets, uint256 usdAssets, uint256 lastUpdatedAt)
+    {
+        uint256 underlyingAmount = VAULT.convertToAssets(shares);
+        (rzrAssets, usdAssets, lastUpdatedAt) = UNDERLYING_ORACLE.getPriceForAmount(underlyingAmount);
+    }
+}
